17db3d09c9cac740addcdbe7386e3692f2404f85
llBlackShadowll <jvelitquintana2@gmail.com>
2024-11-28 21:23:05 -0500

    Descripción de los cambios realizados

--- app/src/main/java/com/example/prototipo/MainActivity.kt
+++ app/src/main/java/com/example/prototipo/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.prototipo
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.example.prototipo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Configurar el DrawerToggle
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Hacer clic en el botón Hamburger
+        binding.imgHamburger.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        // Manejar las opciones del menú del Drawer
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_inicio -> {
+                    // Acción para Inicio
+                }
+                R.id.menu_reserva -> {
+                    // Acción para Reserva
+                }
+                R.id.menu_salir -> {
+                    finish() // Cierra la aplicación
+                }
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
